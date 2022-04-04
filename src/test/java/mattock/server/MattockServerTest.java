20fba2d14167b071c417222ffef0ba77fa2e7a1e
Dave Chesser <me@dchesser.website>
2022-04-04 09:50:24 -0400

    init: new Micronaut project

---
+++ src/test/java/mattock/server/MattockServerTest.java
@@ -0,0 +1,21 @@
+package mattock.server;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MattockServerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
